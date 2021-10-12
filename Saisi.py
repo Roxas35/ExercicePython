ec4eb4f98406a681f8b81220d412052b89a21c4c
Raphaël Cadot <raphael.c@institutsolacroup.com>
2021-10-12 02:57:28 +0200

    Saisi.c

---
+++ Saisi.py
@@ -0,0 +1,9 @@
+# Exercice 1
+
+# Faire la multiplication de deux entiers
+
+# Création des deux variables, faire la multiplication et afficher le résultat.
+nombre1 = input("Le premier nombre est : ")
+nombre2 = input("Le deuxième nombre est : ")
+s = int(nombre1) * int(nombre2)
+print("le résultat de {0} et {1} est de : {2} ".format(nombre1, nombre2, s))
